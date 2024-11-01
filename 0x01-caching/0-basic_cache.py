d3fe6cbb71507f20496d0bc6c73b581c21d68edc
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-11-01 19:38:18 +0300

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Module defines `BasicCache` caching system"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Defines basic caching system
+
+    Inherits
+        - BaseCaching: The class defines the base caching implementation
+    """
+
+    def put(self, key, item):
+        """Add data in cache"""
+        if key is None or item is None:
+            return
+        self.cache_data.update(dict(key=item))
+
+    def get(self, key):
+        """Gets data from cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
